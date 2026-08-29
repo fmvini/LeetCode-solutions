6cdc4dfed6e535dc8407514aedf5893037b06262
Vinícius F. Marrocos <viniciusmarrocos7@gmail.com>
2026-08-29 18:59:22 -0300

    Create 1365-HowManyNumbersAreSmallerThantheCurrentNumber

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.py
@@ -0,0 +1,12 @@
+# Last updated: 29/08/2026, 18:59:21
+1class Solution:
+2    def smallerNumbersThanCurrent(self, nums):
+3        sorted_nums = sorted(nums)
+4
+5        count = {}
+6
+7        for i, num in enumerate(sorted_nums):
+8            if num not in count:
+9                count[num] = i
+10
+11        return [count[num] for num in nums]
